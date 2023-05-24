ed0613c6a71c5bdff79c6fdb93e9412e827250be
foldover <stiskindanil@gmail.com>
2023-05-24 20:15:32 +0300

    v2.0

---
+++ scripts/faq.js
@@ -0,0 +1,119 @@
+function show_list() {
+    var hide = document.getElementById("hide");
+    var arrow1 = document.getElementById("arrow1");
+    if (hide.style.display == "block") {
+      hide.style.display = "none";
+      arrow1.classList.remove("activearrow");
+    } else {
+      arrow1.classList.add("activearrow");
+      hide.style.display = "block";
+    }
+  }
+  function show_list2() {
+    var hide2 = document.getElementById("hide2");
+    var arrow2 = document.getElementById("arrow2");
+    if (hide2.style.display == "block") {
+      arrow2.classList.remove("activearrow");
+      hide2.style.display = "none";
+    } else {
+      arrow2.classList.add("activearrow");
+      hide2.style.display = "block";
+    }
+  }
+
+  function show_list3() {
+    var hide3 = document.getElementById("hide3");
+    var arrow3 = document.getElementById("arrow3");
+    if (hide3.style.display == "block") {
+      arrow3.classList.remove("activearrow");
+      hide3.style.display = "none";
+    } else {
+      arrow3.classList.add("activearrow");
+      hide3.style.display = "block";
+    }
+  }
+
+  function show_list4() {
+    var hide4 = document.getElementById("hide4");
+    var arrow4 = document.getElementById("arrow4");
+    if (hide4.style.display == "block") {
+      arrow4.classList.remove("activearrow");
+      hide4.style.display = "none";
+    } else {
+      arrow4.classList.add("activearrow");
+      hide4.style.display = "block";
+    }
+  }
+
+  function show_list5() {
+    var hide5 = document.getElementById("hide5");
+    var arrow5 = document.getElementById("arrow5");
+    if (hide5.style.display == "block") {
+      arrow5.classList.remove("activearrow");
+      hide5.style.display = "none";
+    } else {
+      arrow5.classList.add("activearrow");
+      hide5.style.display = "block";
+    }
+  }
+
+  function show_list6() {
+    var hide6 = document.getElementById("hide6");
+    var arrow6 = document.getElementById("arrow6");
+    if (hide6.style.display == "block") {
+      arrow6.classList.remove("activearrow");
+      hide6.style.display = "none";
+    } else {
+      arrow6.classList.add("activearrow");
+      hide6.style.display = "block";
+    }
+  }
+
+  function show_list7() {
+    var hide7 = document.getElementById("hide7");
+    var arrow7 = document.getElementById("arrow7");
+    if (hide7.style.display == "block") {
+      arrow7.classList.remove("activearrow");
+      hide7.style.display = "none";
+    } else {
+      arrow7.classList.add("activearrow");
+      hide7.style.display = "block";
+    }
+  }
+
+  function show_list8() {
+    var hide8 = document.getElementById("hide8");
+    var arrow8 = document.getElementById("arrow8");
+    if (hide8.style.display == "block") {
+      arrow8.classList.remove("activearrow");
+      hide8.style.display = "none";
+    } else {
+      arrow8.classList.add("activearrow");
+      hide8.style.display = "block";
+    }
+  }
+
+
+  function show_list9() {
+    var hide9 = document.getElementById("hide9");
+    var arrow9 = document.getElementById("arrow9");
+    if (hide9.style.display == "block") {
+      arrow9.classList.remove("activearrow");
+      hide9.style.display = "none";
+    } else {
+      arrow9.classList.add("activearrow");
+      hide9.style.display = "block";
+    }
+  }
+
+  function show_list10() {
+    var hide10 = document.getElementById("hide10");
+    var arrow10 = document.getElementById("arrow10");
+    if (hide10.style.display == "block") {
+      arrow10.classList.remove("activearrow");
+      hide10.style.display = "none";
+    } else {
+      arrow10.classList.add("activearrow");
+      hide10.style.display = "block";
+    }
+  }
